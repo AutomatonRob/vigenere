158a51975f32293917cc38f227c8d16bcfaec14a
automatonrob <dev@automatonrob.de>
2023-12-05 23:56:46 +0100

    Added Kasiski-Test and IC; still WIP

--- letter_frequencies.py
+++ letter_frequencies.py
@@ -1,4 +1,31 @@
 
+### Frequency of letters in a german text after 
+### Wätjen, Dietmar (2018): Kryptographie. Wiesbaden: Springer Fachmedien Wiesbaden.
 LETTER_FREQUENCIES_GERMAN = {
-    ""
+    "A": 6.43,
+    "B": 1.85,
+    "C": 3.26,
+    "D": 5.12,
+    "E": 17.74,
+    "F": 1.56,
+    "G": 2.69,
+    "H": 5.22,
+    "I": 7.60,
+    "J": 0.23,
+    "K": 1.40,
+    "L": 3.49,
+    "M": 2.75,
+    "N": 10.01,
+    "O": 2.39,
+    "P": 0.64,
+    "Q": 0.01,
+    "R": 6.98,
+    "S": 6.88,
+    "T": 5.94,
+    "U": 4.27,
+    "V": 0.64,
+    "W": 1.73,
+    "X": 0.02,
+    "Y": 0.04,
+    "Z": 1.10
 }
--- vigenere.py
+++ vigenere.py
@@ -1,6 +1,11 @@
-### IMPORTS
+### GLOBAL IMPORTS
 import argparse
 import re
+import math
+from collections import Counter
+
+### LOCAL IMPORTS
+from letter_frequencies import LETTER_FREQUENCIES_GERMAN
 
 ### CONSTANTS
 #ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -15,7 +20,7 @@ OUTPUT_ANALYZED_FILE_NAME = "vigenere_message_analyzed.txt"
 
 def debug_print(message):
     if DEBUG:
-        print(f"[DEBUG] {message}")
+        print(f"\n[DEBUG] {message}")
 
 
 def read_file(file_path):
@@ -23,19 +28,11 @@ def read_file(file_path):
         return file.read()
 
 
-def validate_string(input_name, input_string):
-    ### Remove all white spaces, tabs, and new lines from the string using regular expressions
-    debug_print(f"Removing white space characters from {input_name}.")
-    input_string_no_whitespaces = re.sub(r"\s", "", input_string)
-    non_alphabetic_chars = re.search(r"[^A-Za-z]", input_string_no_whitespaces)
+def prepare_input_string(input_name, input_string):
+    debug_print(f"Removing non-alphabet characters from {input_name}.")
+    pattern = re.compile('[^a-zA-Z]')
 
-    ### Check if string contains only characters A-Z and a-z using regular expressions
-    if non_alphabetic_chars is not None:
-        print(f"[ERROR] Illegal characters found in {input_name}! Only A-Z and a-z are allowed. Exiting.")
-        return
-    else: 
-        debug_print(f"{input_name} looks OK. Processing...")
-        return input_string_no_whitespaces
+    return pattern.sub('', input_string)
 
 
 def string_to_upper_case_ascii_array(input_string):
@@ -47,9 +44,8 @@ def shift_enc(message_char_ascii, key_char_ascii):
 
 
 def shift_dec(message_encrypted_char_ascii, key_char_ascii):
-    return abs((message_encrypted_char_ascii - BASE_SHIFT) - (key_char_ascii - BASE_SHIFT)) % 26 + BASE_SHIFT
+    return ((message_encrypted_char_ascii - BASE_SHIFT) - (key_char_ascii - BASE_SHIFT) + 26) % 26 + BASE_SHIFT
 
-    return 1
 
 def ascii_array_to_string(ascii_array):
     return "".join(chr(ascii_code) for ascii_code in ascii_array)
@@ -57,7 +53,7 @@ def ascii_array_to_string(ascii_array):
 
 def process_input_file(file_name, file_type):
     string_raw = read_file(file_name)
-    string = validate_string(file_type, string_raw)
+    string = prepare_input_string(file_type, string_raw)
     
     debug_print(f"Original {file_type}: {string}")
     
@@ -67,11 +63,11 @@ def process_input_file(file_name, file_type):
 def encrypt_vigenere(message_file_name, key_file_name):
     encrypted_message_array = []
     
-    debug_print("Loading message and key file...")
+    debug_print("Loading message and key file")
     message = process_input_file(message_file_name, "Message")
     key = process_input_file(key_file_name, "Key")
 
-    print("[INFO] Encrypting message...")
+    print("[INFO] Encrypting message")
 
     message_ascii_array = string_to_upper_case_ascii_array(message)
     debug_print(f"Message ASCII-Code array: {message_ascii_array}")
@@ -92,17 +88,17 @@ def encrypt_vigenere(message_file_name, key_file_name):
     
     write_string_to_file(OUTPUT_ENCRYPTED_FILE_NAME, encrypted_message_string)
 
-    print(f"[INFO] DONE! Encrypted message saved in: {OUTPUT_ENCRYPTED_FILE_NAME}")
+    print(f"\n[INFO] DONE! Encrypted message saved in: {OUTPUT_ENCRYPTED_FILE_NAME}")
 
 
 def decrypt_vigenere(encrypted_message_file_name, key_file_name):
     decrypted_message_array = []
     
-    debug_print("Loading encrypted message and key file...")
+    debug_print("Loading encrypted message and key file")
     message_encrypted = process_input_file(encrypted_message_file_name, "Encrypted message")
     key = process_input_file(key_file_name, "Key")
 
-    print("[INFO] Decrypting message...")
+    print("[INFO] Decrypting message")
 
     message_encrypted_ascii_array = string_to_upper_case_ascii_array(message_encrypted)
     debug_print(f"Encrypted message ASCII-Code array: {message_encrypted_ascii_array}")
@@ -123,21 +119,35 @@ def decrypt_vigenere(encrypted_message_file_name, key_file_name):
     
     write_string_to_file(OUTPUT_DECRYPTED_FILE_NAME, decrypted_message_string)
 
-    print(f"[INFO] DONE! Decrypted message saved in: {OUTPUT_DECRYPTED_FILE_NAME}")
+    print(f"\n[INFO] DONE! Decrypted message saved in: {OUTPUT_DECRYPTED_FILE_NAME}")
 
 
 def analyze_vigenere(encrypted_message_file_name):
-    debug_print("Loading encrypted message file...")
+    debug_print(f"Loading encrypted message file")
     message_encrypted = process_input_file(encrypted_message_file_name, "Encrypted message")
 
-    print("[INFO] Analyzing message...")
+    print(f"\n[INFO] Analyzing message")
 
-    message_encrypted_ascii_array = string_to_upper_case_ascii_array(message_encrypted)
-    debug_print(f"Encrypted message ASCII-Code array: {message_encrypted_ascii_array}")
+    # message_encrypted_ascii_array = string_to_upper_case_ascii_array(message_encrypted)
+    # debug_print(f"Encrypted message ASCII-Code array: {message_encrypted_ascii_array}")
+    
+    possible_password_lengths = execute_kasiski_test(message_encrypted)
 
+    # Remove all possible password lengths that were calculated more than once
+    possible_password_length_sanitized = {length: count for length, count in possible_password_lengths.items() if count > 1}
 
+    ic_list = {}
 
-    print(f"[INFO] DONE! Analyzed message saved in: {OUTPUT_ANALYZED_FILE_NAME}")
+    for possible_password_length in possible_password_length_sanitized.keys():
+        ic = calculate_mutual_index_of_coincidence(message_encrypted, possible_password_length)
+        ic_list[possible_password_length] = ic
+
+    debug_print(f"Mutual indices of coincidence: {ic_list}")
+
+    # TODO
+    # print(f"\n[INFO] DONE! Analyzed message saved in: {OUTPUT_ANALYZED_FILE_NAME}")
+
+    return
 
 
 def write_string_to_file(output_file_name, encrypted_message):
@@ -145,25 +155,125 @@ def write_string_to_file(output_file_name, encrypted_message):
         file.write(encrypted_message)
 
 
-def calculate_index_of_coincidence(secret_message):
-    print("[INFO] Calculate Index of Coincidence")
+def find_reoccurring_segments(encrypted_message, segment_length):
+    segments = {}
+    reoccurring_segments = {}
+    encrypted_message_length = len(encrypted_message)
 
+    for i in range(encrypted_message_length - segment_length):
+        current_segment = encrypted_message[i:i + segment_length]
+        if current_segment in segments:
+            segments[current_segment].append(i)
+        else:
+            segments[current_segment] = [i]
+
+    for segment, positions in segments.items():
+        if len(positions) > 1:
+            reoccurring_segments[segment] = positions
+
+    return reoccurring_segments
+
+
+def get_distances(positions):
+    return [positions[i + 1] - positions[i] for i in range(len(positions) - 1)]
+
+
+def calculate_greatest_common_divisor(distances):
+    gcd = distances[0]
+
+    for distance in distances[1:]:
+        gcd = math.gcd(gcd, distance)
+
+    return gcd
+
+
+def execute_kasiski_test(encrypted_message, segment_length=3):
+    print(f"\n[INFO] Executing Kasiski-Test on encrypted message")
+    reoccurring_segments = find_reoccurring_segments(encrypted_message, segment_length)
+    distances = {}
+    possible_password_lengths = {}
+
+    print(f"\nThe following segments reoccurred:")
+
+    for segment, positions in reoccurring_segments.items():
+        #debug_print(f"[POSITIONS] {segment}: {positions}")
+
+        distances[segment] = get_distances(positions)
+
+        #debug_print(f"[DISTANCES] {segment}: {distances[segment]}")
+
+        greatest_common_divisor = calculate_greatest_common_divisor(distances[segment])
+
+        #debug_print(f"[GCD] Possible password length: {greatest_common_divisor}")
+
+        if greatest_common_divisor in possible_password_lengths:
+            possible_password_lengths[greatest_common_divisor] += 1
+        else:
+            possible_password_lengths[greatest_common_divisor] = 1
+
+    debug_print(f"Calculated distances between reoccurring segments: {distances}")
+
+    possible_password_lengths_sorted = dict(sorted(possible_password_lengths.items()))
+    # debug_print(f"Sorted dictionary of possible password lengths and their abundance: {possible_password_lengths_sorted}")
+
+    return possible_password_lengths_sorted
+
+
+def calculate_mutual_index_of_coincidence(message_encrypted, password_length):
+    # print("[INFO] Calculating index of coincidence")
+
+    ic_total = 0
+
+    # Create an array with as many empty strings as the assumed password length
+    blocks = ['' for _ in range(password_length)]
+
+    # Add all characters that were encrypted with the same password character to a separate string
+    for i, char in enumerate(message_encrypted):
+        blocks[i % password_length] += char
+
+    for block in blocks:
+        block_length = len(block)
+
+        if block_length < 2:
+            break
+        
+        char_distribution = {char: block.count(char) for char in set(block)}
+        ic_block = sum((count * (count - 1) for count in char_distribution.values())) / (block_length * (block_length - 1))
+
+        ic_total += ic_block
+
+    return ic_total / password_length
+
+
+def calculate_index_of_coincidence(secret_message):
+
+    secret_message_length = len(secret_message)
+    secret_message_letters_count = dict(sorted(Counter(secret_message).items()))
+    index_of_coincidence = sum((count * (count - 1) for count in secret_message_letters_count.values())) / (secret_message_length * (secret_message_length - 1))
+
+    debug_print(f"Index of Coincidence (IC): {index_of_coincidence}")
 
 def main():
     parser = argparse.ArgumentParser(description="Vigenère-Cipher: Message encryption and cryptanalysis tool")
     parser.add_argument("mode", type=str, help="Operation to perform on the provided message [(e)ncrypt, (d)ecrypt, or (a)nalyze)")
     parser.add_argument("message", type=str, help="The message you want to encrypt, decrypt, or analyse.")
-    parser.add_argument("key", type=str, help="The key that should be used to encrypt or decrypt your message. Not needed for mode \"analyze\".")
+    parser.add_argument("--key", type=str, help="The key that should be used to encrypt or decrypt your message. Not needed for mode \"analyze\".")
     args = parser.parse_args()
 
     mode = args.mode
     debug_print(f"Mode: {mode}")
 
     if mode in ["e", "encrypt"]:
+        if not args.key:
+            print("[ERROR] Key is required for encryption! Exiting.")
+            return
         encrypt_vigenere(args.message, args.key)
     elif mode in ["d", "decrypt"]:
+        if not args.key:
+            print("[ERROR] Key is required for decryption! Exiting.")
+            return
         decrypt_vigenere(args.message, args.key)
-    elif mode in ["a", "anylyze"]:
+    elif mode in ["a", "analyze"]:
         analyze_vigenere(args.message)
     else:
         print("[INFO] [ERROR] Unknown mode. Please use (e)ncrypt, (d)ecrypt, or (a)nalyze only.")
